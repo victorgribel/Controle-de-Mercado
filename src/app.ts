979f161a5663f0d9f9ba06ac3955644444d8f476
victor <victorlgribel@gmail.com>
2023-09-27 12:21:11 -0300

    entregaM4

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import { productsRouter } from "./routers/products.routes";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/products", productsRouter);
 
+app.listen(3000);
